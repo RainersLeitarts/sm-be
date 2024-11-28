c6cad0de57fe4d352e7719f309243cbae50b2dce
Rainers <rainers.leitarts4@gmail.com>
2024-11-28 23:33:12 +0200

    feat: started implementing auth

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
-import express, { Request, Response } from "express"
+import express, { json } from "express"
+import cookieParser from "cookie-parser"
 import cors from "cors"
+import dotenv from "dotenv"
 import routes from "./routes/routes"
 const app = express()
 const PORT = 3000
 
+dotenv.config({ path: ".env" });
 app.use(cors())
-
+app.use(json())
+app.use(cookieParser())
 app.use(routes)
 
 app.listen(PORT, () => {
---
+++ src/routes/posts.ts
@@ -0,0 +1,10 @@
+import { Request, Response, Router } from "express";
+
+const router = Router()
+
+router.post("/create", (req: Request, res: Response) => {
+    
+})
+
+
+export default router
